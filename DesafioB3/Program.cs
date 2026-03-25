50c9cc79c1206897de503eb699cf84ef89821ec4
Lucas Maximo <lucasmaximodantas@gmail.com>
2026-03-25 12:12:30 -0300

    new Program

--- DesafioB3/Program.cs
+++ DesafioB3/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace DesafioB3
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string asset;
+            decimal targetToSell;
+            decimal targetToBuy;
+
+            if (args.Length >= 3)
+            {
+                asset = args[0];
+                targetToSell = args[1];
+                targetToBuy = args[2];
+            }
+
+        }
+    }
+}
