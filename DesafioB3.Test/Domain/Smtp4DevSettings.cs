7aa2a525ba4af8e3a6c93f6f3f60e094f56c523b
Lucas Maximo <lucasmaximodantas@gmail.com>
2026-03-26 14:36:40 -0300

    init tests

---
+++ DesafioB3.Test/Domain/Smtp4DevSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioB3.Test.Domain
+{
+    public class Smtp4DevSettings
+    {
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ DesafioB3.Test/IntegrationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioB3.Test
+{
+    public class IntegrationTest
+    {
+        [Fact]
+        public void EmailToSellIntegrationTests()
+        {
+
+        }
+
+        [Fact]
+        public void EmailToBuyIntegrationTests()
+        {
+
+        }
+    }
+}
